debb49b6d14da39d53dffca44ab9d8c3aaeba277
Khushi Nyati <khushinyati57@gmail.com>
2025-04-13 21:20:40 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Build a Mchine Learning Project that can predict Food delivery Time in minutes',
+    author='Khushi Nyati',
+    license='',
+)
